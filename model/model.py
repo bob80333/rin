47d25e106b8cdb1dd8c6a1a27c71d1d661a8c2c1
Eric Engelhart <bob80333@gmail.com>
2023-09-12 11:30:54 -0400

    Fix patching, MUCH faster loss drop, 300 steps > 25k steps

--- model/model.py
+++ model/model.py
@@ -3,6 +3,8 @@ from model.layers import WriteBlock, ReadBlock, ComputationBlock, FFN, ScalarEmb
 import torch
 import torch.nn as nn
 
+from einops.layers.torch import Rearrange
+
 class RINBlock(nn.Module):
     def __init__(self, embedding_width, latent_width, num_layers):
         super().__init__()
@@ -36,11 +38,15 @@ class RIN(nn.Module):
         self.num_blocks = num_blocks
         self.num_layers_per_block = num_layers_per_block
         
-        self.input_conv = nn.Conv2d(3, self.embed_dim, self.patch_size, self.patch_size)
+        self.input_linear = nn.Linear(3 * patch_size * patch_size, embed_dim)
         self.input_ln = nn.LayerNorm(self.embed_dim) # not sure about this, but it's in the paper (https://github.com/crowsonkb mentioned in EAI discord that a similar LN before patching in ViT diffuion models caused some artifacts)
+        self.input_rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)', p1 = patch_size, p2 = patch_size)
+
         
         self.output_linear = nn.Linear(self.embed_dim, 3 * self.patch_size * self.patch_size)
         # self.output_ln = nn.LayerNorm(embed_dim) # from same discord convo, mentioned that this is good at least for normal ViT (not sure why)
+        self.output_rearrange = Rearrange('b (h w) (c p1 p2) -> b c (h p1) (w p2)', p1 = patch_size, p2 = patch_size, h = self.size)
+
         
         self.pos_embedding = nn.Parameter(torch.empty(1, self.size * self.size, self.embed_dim))
         nn.init.trunc_normal_(self.pos_embedding, a=-0.02, b=0.02)
@@ -61,9 +67,10 @@ class RIN(nn.Module):
     def forward(self, input, timestep, prev_latents = None):
         
         # encode input into embeddings
-        embeddings = self.input_conv(input)
+        #embeddings = self.input_conv(input)
         # squeeze into 2d
-        embeddings = embeddings.flatten(2).transpose(1, 2)
+        embeddings = self.input_rearrange(input)
+        embeddings = self.input_linear(embeddings)
         # add pos embed & ln
         embeddings = self.input_ln(embeddings) + self.pos_embedding
         
@@ -84,7 +91,8 @@ class RIN(nn.Module):
         # now we have our embeddings, so we can decode them into an image
         patches = self.output_linear(embeddings)
         # now we have to unflatten them
-        image = patches.transpose(1, 2).reshape(-1, 3, self.img_size, self.img_size)
+        #image = patches.transpose(1, 2).reshape(-1, 3, self.img_size, self.img_size)
+        image = self.output_rearrange(patches)
         
         return image, latents[:, :-1]
             
