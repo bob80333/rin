e29275212f2bd4635551482a2d9a3ef43032d11e
Eric Engelhart <bob80333@gmail.com>
2023-09-18 13:06:28 -0400

    Add function for generation

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torchvision
 from tqdm import trange
+
 device = 'cuda'
 
 
@@ -53,6 +54,45 @@ def generate(steps, noise, latents, model, conditioning):
     return x_t
 
 
+# generate n images with label k
+def generate_n_k(model_path, n_images, img_resolution, k, output_folder):
+    model = torch.load(model_path)
+    model.eval()
+
+    n = 0
+
+    stepsize = 64
+    for i in range(n_images // stepsize):
+        noise = torch.randn(stepsize, 16, 3, img_resolution,
+                            img_resolution).to(device)
+        latents = torch.zeros(stepsize, 16, 126, 512).to(device)
+        labels = torch.ones(stepsize).long().to(device) * k
+
+        with torch.inference_mode():
+            images = generate(400, noise, latents, model, labels)
+
+        images = images.cpu() * 0.5 + 0.5
+
+        for j in range(stepsize):
+            torchvision.utils.save_image(images[j], f"{output_folder}/{n}.png")
+            n += 1
+
+    # generate remainder
+    noise = torch.randn(n_images % stepsize, 16, 3,
+                        img_resolution, img_resolution).to(device)
+    latents = torch.zeros(n_images % stepsize, 16, 126, 512).to(device)
+    labels = torch.ones(n_images % stepsize).long().to(device) * k
+
+    with torch.inference_mode():
+        images = generate(400, noise, latents, model, labels)
+
+    images = images.cpu() * 0.5 + 0.5
+
+    for j in range(n_images % stepsize):
+        torchvision.utils.save_image(images[j], f"{output_folder}/{n}.png")
+        n += 1
+
+
 model = torch.load("model.pt")
 
 model.eval()
@@ -61,14 +101,14 @@ noise = torch.randn(10, 16, 3, 32, 32).to(device)
 latents = torch.zeros(10, 16, 126, 512).to(device)
 
 for i in range(10):
-    
+
     labels = torch.ones(16).long().to(device) * i
-    
+
     with torch.inference_mode():
         images = generate(400, noise[i], latents[i], model, labels)
-        
+
     images = images.cpu() * 0.5 + 0.5
-    
+
     torchvision.utils.save_image(images, f"images_output/{i}.png", nrow=4)
-    
+
 model.train()
